a7ebf19e5c28beb9288946a12a1b359064407967
Pooja <poojarumugam@gmail.com>
2025-05-08 21:57:03 +0530

    basic java program

---
+++ 1.5.25/areaAndPerimeter.java
@@ -0,0 +1,12 @@
+public class areaAndPerimeter {
+    public static void main(String[] args) {
+        int length = 5;
+        int width = 3;
+
+        int area = length * width;
+        int perimeter = 2 * (length + width);
+
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
